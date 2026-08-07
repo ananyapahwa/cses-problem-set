f9f774447e8fad448fde879318bff2326c72eb73
ananyapahwa <ananyapahwa82@gmail.com>
2026-08-07 13:47:42 +0530

    Update Coin Combinations 2 solutions

---
+++ dynamic_programming/coin_combinations2.cpp/beautifu_optimization.cpp
@@ -0,0 +1,28 @@
+//DO AGAINNNNNNN
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    vector<int> coins(n);
+    for (int &coin : coins)
+        cin >> coin;
+
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1;
+
+    for (int coin : coins) {
+        for (int sum = coin; sum <= x; sum++) {
+            dp[sum] = (dp[sum] + dp[sum - coin]) % MOD;
+        }
+    }
+
+    cout << dp[x] << '\n';
+    return 0;
+}
--- dynamic_programming/coin_combinations2.cpp/coin_combinations_unoptimised.cpp
+++ dynamic_programming/coin_combinations2.cpp/coin_combinations_unoptimised.cpp
@@ -1,35 +1,66 @@
+/*Your loops are:
+
+for (curr = x-1 ... 0)        // x iterations
+    for (idx = n-1 ... 0)     // n iterations
+        for (i = idx ... n-1) // up to n iterations
+
+So the complexity is
+
+O(x×n×n)=O(xn^2)
+With CSES constraints
+n≤100
+x≤10^6
+
+Worst case:
+
+10^6 × 100 × 100
+= 10^10 operations
+
+Ten billion iterations.
+
+That will not finish within the time limit. */
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 const int MOD = 1e9+7;
-// int solve(vector<int>& coins, int x, int curr){
-//     if(curr == x) return 1;
-//     if(curr > x) return 0;
+int solve(vector<int>& coins, int x, int curr, int idx){
+    if(curr == x) return 1;
+    if(curr > x) return 0;
     
-//     int ans = 0;
-//     for(int i=0; i<coins.size(); i++){
-//         ans += solve(coins, x, curr+coins[i]);
-//     }
+    int ans = 0;
+    for(int i=idx; i<coins.size(); i++){
+        ans += solve(coins, x, curr+coins[i], i);
+    }
     
-//     return ans;
-// }
+    return ans;
+}
 int main() {
 	int n, x;
 	cin>>n>>x;
 	vector<int>coins(n);
-	for(int i=0; i<n;i++) cina>>coins[i];
+	for(int i=0; i<n;i++) cin>>coins[i];
 	
-	vector<int> dp(x+1, 0);
-	dp[x] = 1;
+	vector<vector<int>>dp(x+1, vector<int>(n, 0));
+	for (int idx = 0; idx < n; idx++)
+        dp[x][idx] = 1;
+        
+        
 	for(int curr = x-1; curr >= 0; curr--){
-	    int ans = 0;
-	    for(int i=0; i<coins.size(); i++){
+	    for (int idx = n-1; idx >= 0; idx--){
+	        int ans = 0;
+	    for(int i=idx; i<coins.size(); i++){
 	        if(curr + coins[i] <= x)
-                 ans = (ans + dp[curr+coins[i]])%MOD;
+                 ans = (ans + dp[curr+coins[i]][i])%MOD;
             }
-            
-            dp[curr] = ans;
+           
+            dp[curr][idx] = ans;
+	    }
 	  }
-	cout<<dp[0];
+	cout<<dp[0][0];
+
+// cout<<solve(coins, x, 0, 0);
 
 }
