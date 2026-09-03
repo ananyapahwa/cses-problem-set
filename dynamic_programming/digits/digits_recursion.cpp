a6e6fc66a6f42765bf69d73df33d357e86cc3baf
ananyapahwa <ananyapahwa82@gmail.com>
2026-09-03 12:25:24 +0530

    Update digits recursion solution

--- dynamic_programming/digits/digits_recursion.cpp
+++ dynamic_programming/digits/digits_recursion.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int solve(vector<int>& digits, int n){
-    if(n==0) return 1;
+
+vector<int> findDigits(int n){
+    vector<int> digits;
+    while(n!= 0){
+	   digits.push_back(n%10);
+	    n /= 10;
+	}
+	
+	return digits;
+}
+int solve( int n){
+    if(n==0) return 0;
     int ans = INT_MAX;
-    
+    vector<int> digits = findDigits(n);
     for(int i = 0; i<digits.size(); i++){
-        if( n >= digits[i])
-             ans = min(ans, 1+solve(digits, n-digits[i]));
+        if( digits[i]!= 0 && n >= digits[i])
+             ans = min(ans, 1+solve(n-digits[i]));
     }
     
     return ans;
@@ -15,15 +25,8 @@ int main() {
 	// your code goes here
 	int n;
 	cin>>n;
-	int temp = n;
-	//making digits array
-	vector<int> digits;
-	while(temp!= 0){
-	   digits.push_back(temp%10);
-	   temp /= 10;
-	}
 	int ans = 0;
-    ans = solve(digits, n);
+    ans = solve( n);
 	cout<<ans<<endl;
 	
 
